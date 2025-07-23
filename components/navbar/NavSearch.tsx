a9d3e189eb44b121d69adc4a26eb6761e439ff28
Jim124 <fengpingan065@gmail.com>
2025-07-23 15:30:45 +0100

    initialize prisma and mongodb

---
+++ components/navbar/NavSearch.tsx
@@ -0,0 +1,11 @@
+import { Input } from '../ui/input';
+const NavSearch = () => {
+  return (
+    <Input
+      type='search'
+      placeholder='search product...'
+      className='max-w-xs dark:bg-muted '
+    />
+  );
+};
+export default NavSearch;
