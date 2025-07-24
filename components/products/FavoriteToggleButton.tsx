1b6b4462900b4114f30a1707eb96c99bc0970ad2
Jim124 <fengpingan065@gmail.com>
2025-07-24 12:45:40 +0100

    update gitignore file

--- components/products/FavoriteToggleButton.tsx
+++ components/products/FavoriteToggleButton.tsx
@@ -1,4 +1,11 @@
-const FavoriteToggleButton = () => {
-  return <div>FavoriteToggleButton</div>;
+import { FaHeart } from 'react-icons/fa';
+import { Button } from '@/components/ui/button';
+const FavoriteToggleButton = ({ productId }: { productId: string }) => {
+  console.log(productId);
+  return (
+    <Button size='icon' variant='outline' className='p-2 cursor-pointer'>
+      <FaHeart />
+    </Button>
+  );
 };
 export default FavoriteToggleButton;
